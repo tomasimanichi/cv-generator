047234a016b3ee660c9f8cb424c1ca281306674d
tomasimanichi <tomasdoamaral@gmail.com>
2025-11-04 23:39:08 -0300

    integração de validação

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -19,26 +19,20 @@
 	}, false);
 })();
 
-// Máscara simples para telefone brasileiro (formatação ao digitar)
-document.addEventListener('DOMContentLoaded', function() {
-	var phoneInputs = document.querySelectorAll('input[type="tel"]');
-	phoneInputs.forEach(function(input) {
-		input.addEventListener('input', function(e) {
-			var value = e.target.value.replace(/\D/g, '');
+document.addEventListener('input', function (e) {
+	var target = e.target;
+	if (!target || !target.matches || !target.matches('input[type="tel"]')) return;
 
-			// Formatar DDD + número (suporta 8 ou 9 dígitos locais)
-			if (value.length > 2) {
-				value = value.replace(/^([0-9]{2})([0-9]+)$/, '($1) $2');
-				// Separador de sufixo (4 ou 5 dígitos)
-				if (value.replace(/\D/g, '').length > 10) {
-					// formato (DD) 9XXXX-XXXX
-					value = value.replace(/(\d{2})\D*(\d{5})(\d{4})/, '($1) $2-$3');
-				} else {
-					// formato (DD) XXXX-XXXX
-					value = value.replace(/(\d{2})\D*(\d{4})(\d{4})/, '($1) $2-$3');
-				}
-			}
-			e.target.value = value;
-		});
-	});
-});
+	var value = target.value.replace(/\D/g, '');
+
+	if (value.length > 2) {
+		value = value.replace(/^([0-9]{2})([0-9]+)$/, '($1) $2');
+		if (value.replace(/\D/g, '').length > 10) {
+			value = value.replace(/(\d{2})\D*(\d{5})(\d{4})/, '($1) $2-$3');
+		} else {
+			value = value.replace(/(\d{2})\D*(\d{4})(\d{4})/, '($1) $2-$3');
+		}
+	}
+
+	target.value = value;
+}, false);
